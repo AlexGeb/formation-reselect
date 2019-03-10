1e2df54d23dfd24bef9bb497e1a22cd4db81322d
Alexandre Behaghel <alex.behaghel@gmail.com>
2019-03-10 22:53:24 +0100

    Show chained selectors

--- src/Users.js
+++ src/Users.js
@@ -2,18 +2,21 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
 
-export const Users = ({ users }) => {
+export const Users = ({ users, userNames }) => {
   console.log(
     'render Users,',
     'usersSelector number of recomputations : ',
     usersSelector.recomputations()
   );
   return (
-    <ul>
-      {users.map(user => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+      <p>User names : {userNames.join(', ')}</p>
+    </>
   );
 };
 
@@ -22,6 +25,14 @@ const usersSelector = createSelector(
   users => users.filter(user => user.teamId === 2)
 );
 
-const mapStateToProps = state => ({ users: usersSelector(state) });
+const userNamesSelector = createSelector(
+  [usersSelector],
+  users => users.map(user => user.name)
+);
+
+const mapStateToProps = state => ({
+  users: usersSelector(state),
+  userNames: userNamesSelector(state)
+});
 
 export default connect(mapStateToProps)(Users);
